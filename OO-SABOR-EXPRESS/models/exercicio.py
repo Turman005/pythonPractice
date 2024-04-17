291c01bef59c1aac3fa4f417edfab3c948453dc0
Gabriel Turman <80403351+Turman005@users.noreply.github.com>
2024-04-17 09:49:15 -0300

    Add files via upload

---
+++ OO-SABOR-EXPRESS/models/exercicio.py
@@ -0,0 +1,49 @@
+
+# class Musica:
+#     nome = ''
+#     artista = ''
+#     duracao = int
+
+# musica1 = Musica()
+# musica1.artista = 'Skillet'
+# musica1.nome = 'Monster'
+# musica1.duracao = 412
+
+# musica2 = Musica()
+# musica2.nome = 'Atos 2'
+# musica2.artista = 'Gabriela Rocha'
+# musica2.duracao = 412
+
+# musica3 = Musica()
+# musica3.nome = 'Moises'
+# musica3.artista = 'Fernandinho'
+# musica3.duracao = 345
+
+class Restaurante: 
+#     nome = 'Bistro'
+#     categoria = 'teste'
+#     ativo = False
+
+# restaurante_praca = Restaurante()
+# restaurante_praca.nome = 'Praça'
+# restaurante_praca.categoria = 'Italiana'
+# restaurante_praca.ativo = True
+
+
+# restaurante_pizza = Restaurante()
+# restaurante_pizza.nome = 'Pizza Place'
+# restaurante_pizza.categoria = 'Fast Food'
+# restaurante_pizza.ativo = True
+
+# restaurantes = [restaurante_pizza, restaurante_praca]
+
+# print(f'Nome do restaurante: {restaurante_praca.nome}')
+
+# if restaurante_praca.ativo == True:
+#     print(f'O Restaurante {restaurante_praca.nome} atualmente esta Ativo')
+# else:
+#     print(f'O Restaurante {restaurante_praca.nome} atualmente esta inativo')
+
+# categoria = Restaurante.categoria
+
+# print(f'Nome: {restaurante_praca.nome} \nCategoria: {restaurante_praca.categoria} ')
---
+++ OO-SABOR-EXPRESS/models/restaurante.py
@@ -0,0 +1,15 @@
+class Restaurante: 
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+restaurantes = [restaurante_pizza, restaurante_praca]
+
+print(restaurante_praca)
+print(restaurante_pizza)
+
+
