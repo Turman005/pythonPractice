e6c27736bba511731bbefc359f0066ea1a7a63de
Gabriel Turman <80403351+Turman005@users.noreply.github.com>
2024-04-18 11:09:18 -0300

    Add files via upload

--- OO-SABOR-EXPRESS/models/exercicio.py
+++ OO-SABOR-EXPRESS/models/exercicio.py
@@ -1,4 +1,3 @@
-
 # class Musica:
 #     nome = ''
 #     artista = ''
@@ -19,7 +18,7 @@
 # musica3.artista = 'Fernandinho'
 # musica3.duracao = 345
 
-class Restaurante: 
+# class Restaurante: 
 #     nome = 'Bistro'
 #     categoria = 'teste'
 #     ativo = False
@@ -28,7 +27,6 @@ class Restaurante:
 # restaurante_praca.nome = 'Praça'
 # restaurante_praca.categoria = 'Italiana'
 # restaurante_praca.ativo = True
-
 
 # restaurante_pizza = Restaurante()
 # restaurante_pizza.nome = 'Pizza Place'
@@ -47,3 +45,33 @@ class Restaurante:
 # categoria = Restaurante.categoria
 
 # print(f'Nome: {restaurante_praca.nome} \nCategoria: {restaurante_praca.categoria} ')
+
+# class Guerreiro: 
+#     def __init__(self, nome, regiao, raca, arma):
+#         self.nome = nome
+#         self.regiao = regiao
+#         self.raca = raca 
+#         self.arma = arma
+
+# class Mago:
+#     def __init__(self, nome, regiao, raca, atributo):
+#         self.nome = nome
+#         self.regiao = regiao
+#         self.raca = raca
+#         self.atributo = atributo
+
+# guerreiro1 = Guerreiro('Gerald', 'Weldinburg', 'Humano', 'Espada longa')
+# guerreiro2 = Guerreiro('Anya', 'Wolfsburn', 'Elfa', 'Espadas Duplas')
+# guerreiro3 = Guerreiro('Darkness', 'Pitsburg', 'Humano', 'Alabardas Duplas' )
+
+# mago1 = Mago('Dumbledore', 'Hogwarts', 'Humano', 'Fogo')
+# mago2 = Mago('Mashle', 'Reino Magico', 'Humano', 'SARADÃO')
+# mago3 = Mago('Gandalf', 'Terra Media', 'Humano', 'CINZENTO')
+
+# print(vars(guerreiro1))
+# print(vars(guerreiro2))
+# print(vars(guerreiro3))
+# print(vars(mago1))
+# print(vars(mago2))
+# print(vars(mago3))
+
